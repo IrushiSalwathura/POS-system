f96312cc1f8b802e8f6618f5b1ea7274e21e48bd
Chinthani <chinthani96@gmail.com>
2020-07-18 02:24:32 +0530

    Add CustomerForm and CustomerFormController

---
+++ src/controller/CustomerFormController.java
@@ -0,0 +1,26 @@
+package controller;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableView;
+
+public class CustomerFormController {
+
+    public JFXTextField txtID;
+    public JFXTextField txtName;
+    public JFXTextField txtAddress;
+    public TableView tblCustomers;
+    public JFXButton btnAdd;
+    public JFXButton btnSave;
+    public JFXButton btnDelete;
+
+    public void btnAdd_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnSave_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnDelete_OnAction(ActionEvent actionEvent) {
+    }
+}
