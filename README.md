aad364392f2374882c313ffbb298a16ff6af74e8
IrushiSalwathura <irushitharindi@gmail.com>
2020-07-17 20:33:51 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# POS-SYSTEM
+#### This project consists of four main sections;
+1. Customer Management
+2. Item Management
+3. Placing an Order
+4. Searching an order
+#### Please make sure to include the following dependencies in the project folder of your local repository in order to operate and run the system appropriately.
+#####       jfoenix-8.0.8.jar 
---
+++ src/controller/MainFormController.java
@@ -0,0 +1,17 @@
+package controller;
+
+import javafx.event.ActionEvent;
+
+public class MainFormController {
+    public void btnManageCustomer_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnManageItem_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnPlaceOrder_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnSearchOrder_OnAction(ActionEvent actionEvent) {
+    }
+}
